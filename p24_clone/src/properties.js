a314183086fd4ff113e8fa451704135a2368cbf5
Kealym Bromley <kealymmb@gmail.com>
2019-09-17 08:59:25 +0200

    Fixed link message

--- p24_clone/src/properties.js
+++ p24_clone/src/properties.js
@@ -12,19 +12,20 @@ class properties extends React.Component {
     return (
       <div class="row">
         <div class="col s12 m6">
-          <Link to = "/Property">
+          
             <div class="card blue-grey darken-1 hoverable">
+            <Link to = "/Property">
               <div class="card-content white-text">
                 <span class="card-title">Card Title</span>
                 <p>I am a very simple card. I am good at containing small bits of information.
                 I am convenient because I require little markup to use effectively.</p>
               </div>
-          
+            </Link>
               <div>
                 <PropPlace/>
               </div>
             </div>
-          </Link>
+          
         </div>
 
         <div class="col s12 m6">
