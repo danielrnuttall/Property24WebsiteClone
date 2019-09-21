af7a783cd667aa331ff1e71e2affb530a63c0661
Kealym <54358809+KealymB@users.noreply.github.com>
2019-09-21 14:00:57 +0200

    deleted database

--- p24_clone/database/models/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/p24_database', {useNewUrlParser: true}, (err) =>{
-    if(!err) { console.log('MongoDB Connection successful.')}
-});
-
-require('./customers');
