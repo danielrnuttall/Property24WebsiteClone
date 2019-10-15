b2049987dbaf2420de1dcbc24aaa10e8ce305609
Kealym <54358809+KealymB@users.noreply.github.com>
2019-10-15 22:14:28 +0200

    integrated search

--- p24_clone/src/FindProperty.js
+++ p24_clone/src/FindProperty.js
@@ -5,17 +5,28 @@ import '../node_modules/materialize-css/dist/js/materialize.min.js'
 
 import './App.css';
 
+import Properties from './properties';
+
 
 
 function FindProperty() {
+  const [show, setShow] = React.useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
+    
     <form class="col s12">
         <div class="input-field col s6">
-            <input placeholder="Search for a City, Suburb or Web Ref..." id="search" type="text" class="white-text"/>
+            <input placeholder="Search for a City, Suburb or Web Ref..." id="search" type="text" class="black-text white mt-2"/>
             <label for="search" class="white-text">For sale</label>
         </div>
 
-        <a class="waves-effect white-text btn red mt-4">button</a>
+        <a class="waves-effect white-text btn red mt-4" onClick={handleShow}>Search</a>
+
+        { show ? <Properties /> : null }
+
     </form>
   );
 }
